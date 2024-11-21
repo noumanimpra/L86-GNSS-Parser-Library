1370c08a0d2fb0fe3cdc5b7a0e860c2e6ba1d2b2
Nouman <100492073+noumanimpra@users.noreply.github.com>
2024-11-21 13:11:03 +0300

    Add files via upload

---
+++ usr_general.h
@@ -0,0 +1,23 @@
+/*
+ * usr_general.h
+ *
+ *  Created on: Jul 26, 2023
+ *      Author: numan
+ */
+
+#ifndef INC_USR_GENERAL_H_
+#define INC_USR_GENERAL_H_
+#include "stdio.h"
+#include "stm32f0xx_hal.h"
+#include "main.h"
+//#include "dma.h"
+//#include "usart.h"
+//#include "gpio.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "GpsLib.h"
+
+
+#endif /* INC_USR_GENERAL_H_ */
